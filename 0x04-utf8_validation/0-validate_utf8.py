80b0b18a9a0a5d4087001783288f9b5e090d98ef
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-02-01 21:35:31 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,10 +15,8 @@ def validUTF8(data):
                 num_bytes = 3
             elif (i >> 7):
                 return False
-
         else:
             if (i >> 6) != 0b10:
                 return False
             num_bytes -= 1
-
     return num_bytes == 0
