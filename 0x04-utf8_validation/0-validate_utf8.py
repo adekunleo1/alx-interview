c8e92a4f841eb9b2aca68a50fe4a753935e590fc
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-02-01 21:21:16 +0100

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""UTF-8 encoding"""
+
+
+def validUTF8(data):
+    num_bytes = 0
+
+    for i in data:
+        if num_bytes == 0:
+            if (i >> 5) == 0b110:
+                num_bytes = 1
+            elif (i >> 4) == 0b1110:
+                num_bytes = 2
+            elif (i >> 3) == 0b11110:
+                num_bytes = 3
+            elif (i >> 7):
+                return False
+
+        else:
+            if (i >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
