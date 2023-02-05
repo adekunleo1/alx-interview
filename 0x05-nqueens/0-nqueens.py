53b3f419c18f608c459a1b47b0ac1591edb651b7
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-02-05 02:09:22 +0100

    N queens challenge

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,37 +5,34 @@ N queens puzzle challenge
 
 import sys
 
-
-"""Returns The size of dashboard
-"""
 if len(sys.argv) != 2:
     print('Usage: nqueens N')
-    sys.exit(1)
+    exit(1)
 
 try:
-    n = int(sys.argv[1])
-except Exception:
+    n_q = int(sys.argv[1])
+except ValueError:
     print('N must be a number')
-    sys.exit(1)
+    exit(1)
 
-if n < 4:
+if n_q < 4:
     print('N must be at least 4')
-    sys.exit(1)
+    exit(1)
 
 
 def solve_nqueens(n):
-    """self explanatory"""
+    """ self explanatory """
     if n == 0:
         return [[]]
     inner_solution = solve_nqueens(n - 1)
     return [solution + [(n, i + 1)]
-            for i in range(n)
+            for i in range(n_q)
             for solution in inner_solution
             if safe_queen((n, i + 1), solution)]
 
 
 def attack_queen(square, queen):
-    """self explanatory"""
+    """ self explanatory """
     (row1, col1) = square
     (row2, col2) = queen
     return (row1 == row2) or (col1 == col2) or\
@@ -43,14 +40,14 @@ def attack_queen(square, queen):
 
 
 def safe_queen(sqr, queens):
-    """self explanatory"""
+    """ self explanatory """
     for queen in queens:
         if attack_queen(sqr, queen):
             return False
     return True
 
 
-for answer in reversed(solve_nqueens(n)):
+for answer in reversed(solve_nqueens(n_q)):
     result = []
     for p in [list(p) for p in answer]:
         result.append([i - 1 for i in p])
