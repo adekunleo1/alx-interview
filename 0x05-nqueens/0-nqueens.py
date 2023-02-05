5dc7ef5c1d28c4bbd2420725f177d3d08a456f2f
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-02-05 02:13:03 +0100

    N queens Puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -10,12 +10,12 @@ if len(sys.argv) != 2:
     exit(1)
 
 try:
-    n = int(sys.argv[1])
+    n_q = int(sys.argv[1])
 except ValueError:
     print('N must be a number')
     exit(1)
 
-if n < 4:
+if n_q < 4:
     print('N must be at least 4')
     exit(1)
 
@@ -26,7 +26,7 @@ def solve_nqueens(n):
         return [[]]
     inner_solution = solve_nqueens(n - 1)
     return [solution + [(n, i + 1)]
-            for i in range(n)
+            for i in range(n_q)
             for solution in inner_solution
             if safe_queen((n, i + 1), solution)]
 
@@ -47,7 +47,7 @@ def safe_queen(sqr, queens):
     return True
 
 
-for answer in reversed(solve_nqueens(n)):
+for answer in reversed(solve_nqueens(n_q)):
     result = []
     for p in [list(p) for p in answer]:
         result.append([i - 1 for i in p])
