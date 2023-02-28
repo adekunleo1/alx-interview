350bea466650275e17aae0048bc091072e15dca8
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-02-28 02:53:09 +0100

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """ Calculates the fewest number of coins
+        needed to meet a given amount total """
+
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse = True)
+    count = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+        if total == 0:
+            return count
+
+    return -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+ALX Making Changes Interview Project
